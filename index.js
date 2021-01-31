88b96fe9b07a18b1744b01f03274f410f82b61c0
Jeffrey <jeffrey@hismileteeth.com>
2021-02-01 08:07:27 +1000

    [u] test for now

--- index.js
+++ index.js
@@ -108,5 +108,33 @@ const home_app = new Vue({
     }
 })
 
+gsap.registerPlugin(ScrollTrigger)
 
+
+scroller.on('scroll', ScrollTrigger.update)
+
+ScrollTrigger.scrollerProxy(
+    '.container', {
+        scrollTop(value) {
+            return arguments.length ?
+            scroller.scrollTo(value, 0, 0) :
+            scroller.scroll.instance.scroll.y
+        },
+        getBoundingClientRect() {
+            return {
+                left: 0, top: 0, 
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        }
+    }
+)
+
+
+
+
+ScrollTrigger.addEventListener('refresh', () => scroller.update())
+
+
+ScrollTrigger.refresh()
 
